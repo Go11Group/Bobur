9d072196a96f5f894a17e9be326d2caac11be217
bobur <yusupovbobur1001@gmail.com>
2024-06-24 17:05:11 +0500

    uyga vazifa

--- 43-homework/api_gateway_service/api/api.go
+++ 43-homework/api_gateway_service/api/api.go
@@ -12,19 +12,19 @@ func Routes() *http.Server {
 
 	h := handler.NewHandler()
 
-	// api user
+	// api 8090 port un
 	router.POST("/user/create", h.Create)
 	router.PUT("/user/update", h.Update)
 	router.DELETE("/user/delete", h.Delete)
 	router.GET("/user/getbyID/:id", h.GetById)
 	router.GET("/user/getAll", h.GetAll)
 
-	// api card
-	router.POST("/card/create", h.Create)
-	router.PUT("/card/update", h.Update)
-	router.DELETE("/card/delete", h.Delete)
-	router.GET("/card/getbyID/:id", h.GetById)
-	router.GET("/card/getbyAll", h.GetAll)
+	// api 8075 port un
+	router.POST("/create", h.Create1)
+	router.PUT("/update", h.Update1)
+	router.DELETE("/delete", h.Delete1)
+	router.GET("/getbyID/:id", h.GetById1)
+	router.GET("/getbyAll", h.GetAll1)
 	
 
 
--- 43-homework/api_gateway_service/api/handler/handler.go
+++ 43-homework/api_gateway_service/api/handler/handler.go
@@ -22,7 +22,7 @@ func (h *handler) Create(g *gin.Context) {
 	body := g.Request.Body
 
 	client := http.Client{}
-	req, err := http.NewRequest(method, "http://localhost:8090"+url, body)
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, body)
 	if err != nil {
 		fmt.Println(111111)
 		g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
@@ -60,7 +60,7 @@ func (h *handler) Update(g *gin.Context) {
 	url := g.Request.URL.Path
 	body := g.Request.Body
 
-	req, err := http.NewRequest(method, "http://localhost:8090"+url, body)
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, body)
 	if err != nil {
 		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		fmt.Println("error:", err.Error())
@@ -92,7 +92,7 @@ func (h *handler) Delete(g *gin.Context) {
 	method := g.Request.Method
 	url := g.Request.URL.Path
 
-	req, err := http.NewRequest(method, "http://localhost:8090"+url, nil)
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, nil)
 	if err != nil {
 		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		fmt.Println("error:", err.Error())
@@ -118,7 +118,7 @@ func (h *handler) GetById(g *gin.Context) {
 	url := g.Request.URL.Path
 	body := g.Request.Body
 
-	req, err := http.NewRequest(method, "http://localhost:8090"+url, body)
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, body)
 	if err != nil {
 		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		fmt.Println("error:", err.Error())
@@ -153,7 +153,177 @@ func (h *handler) GetAll(g *gin.Context) {
 	url := g.Request.URL.Path
 	body := g.Request.Body
 
-	req, err := http.NewRequest(method, "http://localhost:8090"+url, body)
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, body)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resq, err := client.Do(req)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	defer resq.Body.Close()
+
+	res, err := io.ReadAll(resq.Body)
+	if err != nil {
+
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	g.JSON(http.StatusCreated, string(res))
+}
+
+
+// ------------------------------------
+
+// Create
+func (h *handler) Create1(g *gin.Context) {
+	method := g.Request.Method
+	url := g.Request.URL.Path
+	body := g.Request.Body
+
+	client := http.Client{}
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, body)
+	if err != nil {
+		fmt.Println(111111)
+		g.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println(2222222)
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+
+	res, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(333333333)
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	g.JSON(http.StatusOK, gin.H{"data": string(res)})
+}
+
+// Update
+func (h *handler) Update1(g *gin.Context) {
+	client := http.Client{}
+	method := g.Request.Method
+	url := g.Request.URL.Path
+	body := g.Request.Body
+
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, body)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	res, err := io.ReadAll(resp.Body)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	g.JSON(http.StatusOK, gin.H{"data": string(res)})
+}
+
+// Delete
+func (h *handler) Delete1(g *gin.Context) {
+	client := http.Client{}
+	method := g.Request.Method
+	url := g.Request.URL.Path
+
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, nil)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	resq, err := client.Do(req)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	defer resq.Body.Close()
+
+	g.JSON(http.StatusCreated, "SUCCESS")
+}
+
+// Get by id
+func (h *handler) GetById1(g *gin.Context) {
+	client := http.Client{}
+	method := g.Request.Method
+	url := g.Request.URL.Path
+	body := g.Request.Body
+
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, body)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	resq, err := client.Do(req)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	defer resq.Body.Close()
+
+	res, err := io.ReadAll(resq.Body)
+	if err != nil {
+		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println("error:", err.Error())
+		return
+	}
+
+	g.JSON(http.StatusCreated, res)
+}
+
+// Get all
+func (h *handler) GetAll1(g *gin.Context) {
+	client := http.Client{}
+	method := g.Request.Method
+	url := g.Request.URL.Path
+	body := g.Request.Body
+
+	req, err := http.NewRequest(method, "http://localhost:8075"+url, body)
 	if err != nil {
 		g.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		fmt.Println("error:", err.Error())
